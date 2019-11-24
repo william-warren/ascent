69632031bc05b01f36b8658dc43063e9c6016719
Nate Clark <natec425@gmail.com>
2019-11-24 16:43:46 -0600

    Add login

--- ascent/urls.py
+++ ascent/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
+    path(
+        "login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"
+    ),
 ]
--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 
 class TestUserVisitsHomePage(TestCase):
@@ -6,3 +8,18 @@ class TestUserVisitsHomePage(TestCase):
         response = self.client.get("/")
         self.assertContains(response, "Ascent")
         self.assertContains(response, "<title>Ascent</title>", html=True)
+
+
+class TestUserLogsIn(TestCase):
+    def test_successfully(self):
+        user = User.objects.create_user(
+            "student", "student@basecampcodingacademy.org", "studentpassword"
+        )
+
+        self.client.get("/login/")
+
+        response = self.client.post(
+            "/login/", {"username": "student", "password": "studentpassword"}
+        )
+
+        self.assertEqual(user, auth.get_user(self.client))

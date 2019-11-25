6cc812e91f21dd4d2fafd2b7ccea0d277ae24e2a
Nate Clark <natec425@gmail.com>
2019-11-24 18:35:54 -0600

    Add some basic css and login/logout nav

--- ascent/settings.py
+++ ascent/settings.py
@@ -106,6 +106,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 DEFAULT_FROM_EMAIL = "no-reply@basecampcodingacademy.org"

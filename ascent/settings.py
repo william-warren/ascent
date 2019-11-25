01e4cc1587019cb1807aac0fd06991647ecb15b3
Nate Clark <natec425@gmail.com>
2019-11-24 22:28:34 -0600

    Add bcca-ascent.herokuapp.com to allowed hosts

--- ascent/settings.py
+++ ascent/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ["DEBUG"] == "ON"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["bcca-ascent.herokuapp.com"]
 
 # Application definition
 

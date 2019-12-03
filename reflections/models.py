60c2d343196b5576b0bce832745ba6a55e4602cd
Student <student@donation-3.local>
2019-12-03 14:40:31 -0600

    Added New Models and Created the Reflection App

--- reflections/models.py
+++ reflections/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime
+
 # Create your models here.
 
+
 class Reflection(models.Model):
-    question = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    question = models.ForeignKey(Question, on_delete=models.PROTECT)
+    day_of_reflection = models.DateField()
+
+class Question(models.Model):
+    content = models.TextField()
     answer = models.TextField()
-    user = models.ForeignKey(User, on_delete= models.PROTECT)
-    
+
+class Submission(models.Model)
--- reflections/urls.py
+++ reflections/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 app_name = "reflections"
 
-urlpatterns = [
-]
+urlpatterns = [path("", views.home, name="home")]
 

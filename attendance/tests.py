6b095e7d2bde6055ac6e424c76f99cf140bca2cd
Angie Unda <aunda@basecampcodingacademy.org>
2019-12-04 16:26:52 -0600

    Created tests to verfiy when we successfully completed our first ticket

--- attendance/tests.py
+++ attendance/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
+from django.utils import timezone
 
-# Create your tests here.
+class TestStudentChecksIn(TestCase):
+    def test_successfully(self):
+        user = User.objects.create_user("daffy")
+
+        self.client.force_login(user)
+
+        self.client.post(
+            reverse("attendance:check-in")
+        )
+
+        self.assertEqual(
+            user.checkin_set.count(), 1
+        )
+
+        checkin = user.checkin_set.first()
+
+        self.assertAlmostEqual(check.datetime.timestamp(), timezone.now().timestamp())

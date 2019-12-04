eca9415b8be8f73b2c5831a950aecab735ab0fea
Devin Booker <dbooker@basecampcodingacademy.org>
2019-12-04 15:17:04 -0600

    completed basic feature for the sign_up view which is to let the user make a headline

--- showcase/views.py
+++ showcase/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
-from forms import SignUp
+from .forms import SignUp
 
 def sign_up(request):
     if request.method == "GET":
         return render(request, "profile.html")
     elif request.method == "POST":
         form = SignUp(request.POST)
-        
+        if form.is_valid():
+            headline = form.cleaned_data["headline"]
+            profile = SignUp.objects.create(headline=headline)
+
+
+
+
 
         

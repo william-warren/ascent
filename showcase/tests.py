af131729d2b91f692249260b4dedc1ad531602ae
Devin Booker <dbooker@basecampcodingacademy.org>
2019-12-04 15:13:10 -0600

    Created test case for the user's headline on their profile

--- showcase/tests.py
+++ showcase/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your tests here.
+class TestStudentCreatesProfile(TestCase):
+    def test_successfully(self):
+        user = User.objects.create_user('jimbob')
+
+        self.client.post(
+            reverse("showcase:create-profile"),
+            {"headline": "I like code and such you know"}
+        )
+
+        self.assertEqual(user.profile.headline, "I like code and such you know")

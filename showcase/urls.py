e78f8fa46409cada9e968895120a3fe121371b3d
Devin Booker <dbooker@basecampcodingacademy.org>
2019-12-05 15:22:45 -0600

    Moved the user-profile and made it the root
    Moved the user-profile url to line 7 and made it the root path and then changed name to "profile-list"

--- showcase/urls.py
+++ showcase/urls.py
@@ -4,6 +4,6 @@ from showcase import views
 app_name = "showcase"
 
 urlpatterns = [
+    path("", views.user_profiles, name="profile-list"),
     path("create-profile/", views.sign_up, name="create-profile"),
-    path("user-profile", views.user_profiles, name="user-profile"),
 ]

9f7a9ff94ae8e1705b8dce06fa7558f0a8be76e6
Devin Booker <dbooker@basecampcodingacademy.org>
2019-12-05 15:30:08 -0600

    Imported, fixed names, fixed grammar
    Imported several more things after SignUp, made the sign_up view require user login, assigned user, and fixed all names and syntax

--- showcase/views.py
+++ showcase/views.py
@@ -1,18 +1,25 @@
 from django.shortcuts import render, redirect
-from .forms import SignUp
+from showcase.forms import SignUp
+from showcase.models import Profile
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def sign_up(request):
     if request.method == "GET":
-        return render(request, "create-profile.html")
+        return render(request, "create-profile.html", {"form": SignUp()})
     elif request.method == "POST":
         form = SignUp(request.POST)
         if form.is_valid():
+            user = request.user
             headline = form.cleaned_data["headline"]
-            profile = SignUp.objects.create(headline=headline)
-            return redirect("user_profiles")
+            profile = Profile.objects.create(user=user, headline=headline)
+            return redirect("showcase:profile-list")
+        else:
+            return render(request, "create-profile.html", {"form": form})
 
 def user_profiles(request):
-    profiles = SignUP.objects.all()
+    profiles = Profile.objects.all()
     return render(request, "user-profiles.html", {"profiles": profiles})
 
 

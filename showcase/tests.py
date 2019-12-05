2299388e594b97323a01e3b851a271b4c82a7236
Devin Booker <dbooker@basecampcodingacademy.org>
2019-12-05 15:12:57 -0600

    Had more unit test added to test the code in the showcase directory

--- showcase/tests.py
+++ showcase/tests.py
@@ -5,11 +5,35 @@ from django.urls import reverse
 # Create your tests here.
 class TestStudentCreatesProfile(TestCase):
     def test_successfully(self):
-        user = User.objects.create_user('jimbob')
+        user = User.objects.create_user("jimbob")
+
+        self.client.force_login(user)
 
         self.client.post(
             reverse("showcase:create-profile"),
-            {"headline": "I like code and such you know"}
+            {"headline": "I like code and such you know"},
         )
 
         self.assertEqual(user.profile.headline, "I like code and such you know")
+
+    def test_form_is_rendered_on_create_profile_page(self):
+        user = User.objects.create_user("jimbob")
+        self.client.force_login(user)
+
+        response = self.client.get(reverse("showcase:create-profile"))
+
+        self.assertTemplateUsed(response, "create-profile.html")
+        self.assertIn('form', response.context)
+
+    
+    def test_form_errors_are_rendered_on_invalid_post(self):
+        user = User.objects.create_user("jimbob")
+        self.client.force_login(user)
+
+        response = self.client.post(reverse("showcase:create-profile"))
+
+        self.assertTemplateUsed(response, "create-profile.html")
+        self.assertTrue(response.context['form'].errors)
+
+
+

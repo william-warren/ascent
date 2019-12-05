80fbf5cf93232abd797061850d1f7d52415e3a4b
Devin Booker <dbooker@basecampcodingacademy.org>
2019-12-05 08:27:24 -0600

    Created user_profiles view to see all user created profiles

--- showcase/views.py
+++ showcase/views.py
@@ -1,16 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import SignUp
 
 def sign_up(request):
     if request.method == "GET":
-        return render(request, "profile.html")
+        return render(request, "create-profile.html")
     elif request.method == "POST":
         form = SignUp(request.POST)
         if form.is_valid():
             headline = form.cleaned_data["headline"]
             profile = SignUp.objects.create(headline=headline)
+            return redirect("user_profiles")
 
-
+def user_profiles(request):
+    profiles = SignUP.objects.all()
+    return render(request, "user-profiles.html", {"profiles": profiles})
 
 
 

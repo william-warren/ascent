d1100e59b70129c8142300da6953e7501826ed36
Patton Burge <pburge@basecampcodingacademy.org>
2019-12-05 12:38:59 -0600

    Uncommented out Models,Admin,and Views

--- reflections/admin.py
+++ reflections/admin.py
@@ -1,25 +1,25 @@
 from django.contrib import admin
-# from .models import Reflection, Question, QuestionSubmission, Submission
+from .models import Reflection, Question, QuestionSubmission, Submission
 
 
-# class ReflectionAdmin(admin.ModelAdmin):
-#     pass
+class ReflectionAdmin(admin.ModelAdmin):
+    pass
 
 
-# class QuestionAdmin(admin.ModelAdmin):
-#     pass
+class QuestionAdmin(admin.ModelAdmin):
+    pass
 
 
-# class SubmissionAdmin(admin.ModelAdmin):
-#     pass
+class SubmissionAdmin(admin.ModelAdmin):
+    pass
 
 
-# class QuestionSubmissionAdmin(admin.ModelAdmin):
-#     pass
+class QuestionSubmissionAdmin(admin.ModelAdmin):
+    pass
 
 
-# admin.site.register(Reflection, ReflectionAdmin)
-# admin.site.register(Question, QuestionAdmin)
-# admin.site.register(QuestionSubmission, QuestionSubmissionAdmin)
-# admin.site.register(Submission, SubmissionAdmin)
+admin.site.register(Reflection, ReflectionAdmin)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(QuestionSubmission, QuestionSubmissionAdmin)
+admin.site.register(Submission, SubmissionAdmin)
 
--- reflections/views.py
+++ reflections/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-# from .models import Reflection,Submission
+from .models import Reflection,Submission
 
-# def home(request):
-#     reflections = Reflection.objects.all()
-#     submissions = Submission.objects.all()
-#     return render(request, "reflections/base.html", {"reflections": reflections, "submissions": submissions})
+def home(request):
+    reflections = Reflection.objects.all()
+    submissions = Submission.objects.all()
+    return render(request, "reflections/base.html", {"reflections": reflections, "submissions": submissions})

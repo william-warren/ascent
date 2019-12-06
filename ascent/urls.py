76f6d2513ae16ad15975ce8bd4b65c4b0d0dafc7
JD Gault <jgault@basecampcodingacademy.org>
2019-12-06 11:05:43 -0600

    Add url config back

--- ascent/urls.py
+++ ascent/urls.py
@@ -27,5 +27,6 @@ urlpatterns = [
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("magic-link/", include("magic_links.urls"), name="magic-link"),
     path("reflections/", include("reflections.urls"), name="reflections"),
-    path("mileage_tracker/", include("mileage_tracker.urls"), name="mileage_tracker")
+    path("mileage_tracker/", include("mileage_tracker.urls"), name="mileage_tracker"),
+    path("shoutouts", include("shoutouts.urls"), name="shoutouts")
 ]

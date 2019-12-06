cfeff246921823a8ec2a3f75dff3e626f82617db
Angie Unda <aunda@basecampcodingacademy.org>
2019-12-06 09:35:42 -0600

    We decided to create an admin to ensure our database it updating based on each time a  student checks in.

--- attendance/admin.py
+++ attendance/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from attendance.models import Checkin
 
-# Register your models here.
+
+class CheckinAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Checkin, CheckinAdmin)

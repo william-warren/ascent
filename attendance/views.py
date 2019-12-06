ee015d59a7198935d1ebcca4f30d8156f45482fd
Angie Unda <aunda@basecampcodingacademy.org>
2019-12-06 11:42:03 -0600

    Decided to take out the message that said that specific user has checked in and instead included it into our form.

--- attendance/views.py
+++ attendance/views.py
@@ -6,19 +6,22 @@ from attendance.models import Checkin
 from django.contrib.auth.models import User
 from django.utils import timezone
 
+
 @login_required
 def checkin(request):
     if request.method == "GET":
-        # messages.success(request, "you have checked in successfully!")
         return render(request, "attendance.html")
     elif request.method == "POST":
-            student = request.user
-            messages.success(request, "You are checked in.")
-            checkin = Checkin.objects.create(
-                    student=student, datetime=timezone.now()
-                )
-            checkin.save()
-            return redirect("attendance:check-in")
+        student = request.user
+        checkin = Checkin.objects.create(student=student, datetime=timezone.now())
+        checkin.save()
+        return redirect("attendance:thanks")
     else:
-        return render(request, "attendance.html" , {"form":form})
+        return render(request, "attendance.html", {"form": form})
     return redirect("attendance.html")
+
+
+@login_required
+def thanks(request):
+    if request.method == "GET":
+        return render(request, "thanks.html")

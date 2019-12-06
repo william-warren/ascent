efb219a4d1a4fd828a11488c00b29fa3f8b90aaf
Angie Unda <aunda@basecampcodingacademy.org>
2019-12-06 09:39:53 -0600

    We deleted the form from the view so we had to appopriatly update our views. Also imported timezone for check in time.

--- attendance/views.py
+++ attendance/views.py
@@ -1,33 +1,24 @@
-"""
-
-Create the view that uses a form and our check-in button to sign in the user. 
-
-
-"""
-
 from django.shortcuts import render, redirect
 from django.views import View
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from attendance.models import Checkin
 from django.contrib.auth.models import User
-from attendance.forms import Checkinform
+from django.utils import timezone
 
 @login_required
 def checkin(request):
-    if not user.checked_in:
-        user.checked_in = True
-        user.save()
-        form = Checkinform(request.user, request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, "you have checked in successfully!")
-            return redirect("attendance.html")
-        else:
-            messages.warning(request, "You are already checked.")
-    return render(request, "attendance.html", {"form": form})
-
-
-class Attendance(View):
-    def get(self, request):
+    if request.method == "GET":
+        # messages.success(request, "you have checked in successfully!")
         return render(request, "attendance.html")
+    elif request.method == "POST":
+            student = request.user
+            messages.success(request, "You are checked in.")
+            checkin = Checkin.objects.create(
+                    student=student, datetime=timezone.now()
+                )
+            checkin.save()
+            return redirect("attendance:check-in")
+    else:
+        return render(request, "attendance.html" , {"form":form})
+    return redirect("attendance.html")

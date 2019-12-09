ec2e2d98754e9bf3e4b2ea4f8ff20c93594036a1
Tyler Irving <tirving@basecampcodingacademy.org>
2019-12-09 12:56:57 -0600

    Models.py
    `timezone` was added to the imports
    
    the DriveToWork model was changed to just a `user` & `date` field.
    
    migrations are included

--- mileage_tracker/models.py
+++ mileage_tracker/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from django.utils import timezone
 from django.contrib.auth.models import User
+
 
 class DistanceToWork(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     miles = models.FloatField()
 
-class Commute(models.Model):
+
+class DriveToWork(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    commute = models.TextField()
+    date = models.DateField()

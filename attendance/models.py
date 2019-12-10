93811039fb64d4fbb3d54fa23e878a7d616a17de
Angie Unda <aunda@basecampcodingacademy.org>
2019-12-10 12:54:55 -0600

    Split some of the functions from the view.py to clearly see the difference between the work down to the webpage vs the database.
    Co-authored-by: Dakota Nichols <dakotanichols0996@gmail.com>

--- attendance/models.py
+++ attendance/models.py
@@ -1,8 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from attendance.choices import *
 
 
 class Checkin(models.Model):
     student = models.ForeignKey(User, on_delete=models.CASCADE)
     datetime = models.DateTimeField(default=timezone.now)
+    verified = models.BooleanField(default=False)
+    status = models.IntegerField(choices=STATUS_CHOICES, default=2)
+
+    @staticmethod
+    def checkin_user(user):
+        return user.checkin_set.create()
+
+    @staticmethod
+    def is_user_checked_in(user):
+        return user.checkin_set.filter(datetime__date=timezone.now().date()).exists()
+

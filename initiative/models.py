0767f7a784272ca0829979520d70170e23120fd9
Student <william.warren1019@yahoo.com>
2019-12-12 14:48:25 -0600

    .

--- initiative/models.py
+++ initiative/models.py
@@ -9,7 +9,7 @@ class Initiative(models.Model):
     team_leader = models.ForeignKey(User, on_delete=models.PROTECT)
     completion = models.BooleanField(default=False)
     date = models.DateField(default=timezone.now)
-    yeet = models.TextField()
+
 
 
 class StatusReport(models.Model):
